33dc31c83ad228918d41bff36f97014c5d6fd98b
Gianluca Casati <fibo@users.noreply.github.com>
2025-02-18 14:28:18 +0700

    can play memory-game

--- elements.js
+++ elements.js
@@ -8,12 +8,28 @@ function html(strings, ...expressions) {
   return template
 }
 
+// Use Fisher–Yates shuffle algorithm.
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+function shuffleArray(arr) {
+  let i = arr.length
+  let shuffled = arr.slice(0)
+  let swap, index
+  while (i--) {
+    index = Math.floor((i + 1) * Math.random())
+    swap = shuffled[index]
+    shuffled[index] = shuffled[i]
+    shuffled[i] = swap
+  }
+  return shuffled
+}
+
 /**
  * Display a mora
  * @example
  * <mora-char index="42" display="hiragana"></mora-char>
  */
 export class MoraChar extends HTMLElement {
+  static localName = 'mora-char'
   static get observedAttributes() { return ['index', 'display'] }
   textNode = document.createTextNode('')
   constructor() {
@@ -44,7 +60,7 @@ class BasicChars extends HTMLElement {
           padding: .2em .4em;
         }
         .cell--neutral {
-          background: var(--color-neutral);
+          background: var(--color-neutral-1);
         }
         .cell--start-2 { grid-column-start: 2; }
         .cell--start-4 { grid-column-start: 4; }
@@ -138,52 +154,87 @@ class BasicChars extends HTMLElement {
 }
 
 class MemoryGame extends HTMLElement {
-  selectedRow
-  selectedCol
-  cellSelectedCssClass = 'cell--selected'
+  static numCols = 2
+  static get observedAttributes() { return ['rows'] }
+  static cssClass = {
+    cell: 'cell',
+    cellMatched: 'cell--matched',
+    cellSelected: 'cell--selected',
+    container: 'container',
+  }
+  numMatches = 0
   cells = new Map()
+  cellIndexes = new Map()
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
+    const { cssClass } = MemoryGame
     const template = html`
       <style>
-        .container {
+        .${cssClass.container} {
           display: inline-grid;
           grid-template-columns: 1fr 1fr;
           gap: var(--size-1);
-          background: var(--color-neutral);
+          background: var(--color-neutral-1);
         }
-        .cell {
+        .${cssClass.cell} {
           width: var(--size-2);
           height: var(--size-2);
           background: var(--color-white);
           border: 1px solid transparent;
+          user-select: none;
+          display: grid;
+          place-content: center;
+          transition: all 150ms;
         }
-        .cell--selected {
+        .${cssClass.cellSelected} {
           border-color: var(--color-accent);
         }
+        .${cssClass.cellMatched} {
+          background-color: var(--color-neutral-2);
+          border-color: var(--color-neutral-2);
+        }
       </style>
-      <div class="container">
-      </div>
+      <div class="${cssClass.container}"></div>
     `
     this.shadowRoot.appendChild(template.content.cloneNode(true))
-    this.container = this.shadowRoot.querySelector('.container')
-    const numCols = 2
-    const numRows = 6
-    for (let row = 0; row < numRows; row++)
-      for (let col = 0; col < numCols; col++)
-        this.createCell(row, col)
+    this.container = this.shadowRoot.querySelector(`.${cssClass.container}`)
+  }
+  attributeChangedCallback(name, _, value) {
+    if (name == 'rows') {
+      const numRows = this.numRows = Number(value)
+      const { numCols } = MemoryGame
+      // Remove previous cells and create new ones.
+      this.removeCells()
+      for (let row = 0; row < numRows; row++)
+        for (let col = 0; col < numCols; col++)
+          this.createCell(row, col)
+      // Populate content.
+      this.generateCellsContent()
+    }
   }
   connectedCallback() {
     this.container.addEventListener('click', this)
   }
   handleEvent(event) {
     if (event.type == 'click') {
-      event.stopPropagation()
+      const { cssClass } = MemoryGame
       const { target } = event
+      // If all cells are matched, restart game.
+      if (this.numMatches == this.numRows)
+        return this.restart()
+      // Ignore container.
       if (target === this.container) return
-      const { row, col } = target.dataset
-      const { selectedCol, selectedRow } = this
+      // Search for the cell, starting from the event target.
+      let cell = target
+      while (!cell.classList.contains(cssClass.cell))
+        cell = cell.parentElement
+      // Ignore matched cells.
+      if (cell.classList.contains(cssClass.matchCells)) return
+      event.stopPropagation()
+      const { row, col } = cell.dataset
+      const { selectedCol, selectedRow, selectedIndex } = this
+      const index = this.cellIndexes.get(this.cellKey(row, col))
       if (selectedRow && selectedCol) {
         if (col == selectedCol) {
           if (row == selectedRow) {
@@ -196,14 +247,29 @@ class MemoryGame extends HTMLElement {
           }
         } else {
           // There could be a match.
+          if (index == selectedIndex) {
+            this.matchCells(row, col, selectedRow, selectedCol)
+            this.numMatches++
+          } else {
+            // No match
+          }
         }
         return
       } else {
         // No previous selection, just select cell.
         this.selectCell(row, col)
       }
-      // event.target.classList.toggle
     }
+  }
+  addPair(row1, row2, moraIndex) {
+    const key1 = this.cellKey(row1, 0)
+    const key2 = this.cellKey(row2, 1)
+    const cell1 = this.cells.get(key1)
+    const cell2 = this.cells.get(key2)
+    this.cellIndexes.set(key1, moraIndex)
+    this.cellIndexes.set(key2, moraIndex)
+    cell1.innerHTML = `<mora-char display="hiragana" index="${moraIndex}"></mora-char>`
+    cell2.innerHTML = `<mora-char display="latin" index="${moraIndex}"></mora-char>`
   }
   cellKey(row, col) {
     return `${row}-${col}`
@@ -216,17 +282,65 @@ class MemoryGame extends HTMLElement {
     cell.dataset.col = col
     this.container.appendChild(cell)
   }
-  selectCell(row, col) {
-    const cell = this.cells.get(this.cellKey(row, col))
-    cell.classList.add(this.cellSelectedCssClass)
-    this.selectedRow = row
-    this.selectedCol = col
-  }
-  deselectCell(row, col) {
-    const cell = this.cells.get(this.cellKey(row, col))
-    cell.classList.remove(this.cellSelectedCssClass)
+  restart() {
+    this.numMatches = 0
     this.selectedRow = undefined
     this.selectedCol = undefined
+    this.selectedIndex = undefined
+    this.clearCells()
+    this.generateCellsContent()
+  }
+  selectCell(row, col) {
+    const { cssClass } = MemoryGame
+    const key = this.cellKey(row, col)
+    const cell = this.cells.get(key)
+    cell.classList.add(cssClass.cellSelected)
+    this.selectedRow = row
+    this.selectedCol = col
+    this.selectedIndex = this.cellIndexes.get(key)
+  }
+  clearCells() {
+    const { cssClass, numCols } = MemoryGame
+    const { numRows } = this
+    for (let row = 0; row < numRows; row++)
+      for (let col = 0; col < numCols; col++) {
+        const cell = this.cells.get(this.cellKey(row, col))
+        cell.classList.remove(cssClass.cellMatched)
+        cell.classList.remove(cssClass.cellSelected)
+      }
+  }
+  deselectCell(row, col) {
+    const { cssClass } = MemoryGame
+    const cell = this.cells.get(this.cellKey(row, col))
+    cell.classList.remove(cssClass.cellSelected)
+    this.selectedRow = undefined
+    this.selectedCol = undefined
+    this.selectedIndex = undefined
+  }
+  generateCellsContent() {
+    const { numRows } = this
+    const kanaIndexes = Array.from(Array(kana.length).keys())
+    const firstCol = Array.from(Array(numRows).keys())
+    const secondCol = shuffleArray(firstCol)
+    const moraIndex = shuffleArray(kanaIndexes).slice(0, firstCol.length)
+    for (let row = 0; row < numRows; row++)
+      this.addPair(firstCol[row], secondCol[row], moraIndex[row])
+  }
+  matchCells(row1, col1, row2, col2) {
+    const { cssClass } = MemoryGame
+    this.deselectCell(row1, col1)
+    this.deselectCell(row2, col2)
+    const cell1 = this.cells.get(this.cellKey(row1, col1))
+    const cell2 = this.cells.get(this.cellKey(row2, col2))
+    cell1.classList.add(cssClass.cellMatched)
+    cell2.classList.add(cssClass.cellMatched)
+  }
+  removeCells() {
+    this.cells.clear()
+    this.cellIndexes.clear()
+    const { container } = this
+    while (container.firstChild)
+      container.removeChild(container.lastChild)
   }
 }
 
